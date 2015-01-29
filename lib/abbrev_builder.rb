8cbeafc65bf6fe8710dcbf362a4f153490aee64a
Jeremy D. Acord <jdacronym@gmail.com>
2015-01-28 19:44:31 -0800

    Tree building!
    This was hard, partly owing to the difficulty of putting together
    trees from arrayrefs, but the spec and code now expect an
    Abbrev::Builder to build trees of a certain format. Next: parsing them
    as unambiguous a set of abbreviations as possible
    
    	modified:   lib/abbrev_builder.rb
    	modified:   lib/abbrev_builder_spec.rb

--- lib/abbrev_builder.rb
+++ lib/abbrev_builder.rb
@@ -22,20 +22,48 @@ module Abbrev
 
     def build_tree
       @tree = []
-      @words.each { |word| add_to_subtree(word,@tree) }
+      @words.each { |word| Abbrev::Builder.add_to_tree(word,@tree) }
     end
 
     def traverse_tree
+      Abbrev::Builder.abbrevs_of(@tree)
+    end
+
+    ##this will be futher refactored. Wish I had macros. :|
+    def self.add_to_tree(word,tree = [])
+      first  = word[0]
+      rest   = word[1..word.size]
+      prefix = tree.select { |subtree| subtree && subtree[0] == first }
+      if prefix.empty?
+        if rest.size == 0
+          tree << [ first, [nil] ] 
+        else
+          tree << [ first, add_to_tree(rest) ]
+        end
+      else
+        match = prefix[0]
+        case match.length
+        when 1 
+          if rest.size == 0
+            tree
+          else
+             match << add_to_tree(rest, [first])
+          end
+        when 2
+          if rest.size == 0
+            match[1] << nil unless match[1].one? { |e| e.nil? }
+          else
+            add_to_tree rest, match[1]
+          end
+        else        raise MalformedTreeError
+        end
+      end
+      tree
+    end
+
+    def self.abbrevs_of(tree)
       raise NotImplementedError
     end
 
-    def self.add_to_subtree(word,tree)
-      prefix = tree.select { |e| e[0] == word[0] }
-      if prefix.empty?
-        tree << [ word[0], [word[1..word.size]] ]
-      else
-        add_to_subtree word[1..word.size], prefix[1]
-      end
-    end
   end #Builder
 end #Abbrev
--- lib/abbrev_builder_spec.rb
+++ lib/abbrev_builder_spec.rb
@@ -1,13 +1,15 @@
 require 'rspec'
 require 'abbrev_builder'
 
-describe Abbrev::Builder do
+## alias the class so we don't have to wear our fingers down.
+Builder = Abbrev::Builder
+describe Builder do
   before :each do
-    @builder = Abbrev::Builder.new
+    @builder = Builder.new
   end
 
   it "responds to :new with an instance" do
-    @builder.instance_of?(Abbrev::Builder).should == true
+    @builder.instance_of?(Builder).should == true
   end
 
   it "builds a hash of abbreviations, like abbrev" do
@@ -20,19 +22,19 @@ describe Abbrev::Builder do
 
   it "accepts new words (chainable)" do
     @builder.add_word('new')
-      .instance_of?(Abbrev::Builder).should == true
+      .instance_of?(Builder).should == true
   end
 
   it "generates abbreviations for words it contains" do
-    @builder.add_word('new').build.should.include? 'n'
-    @builder.build.keys.should.include? 'new'
+    @builder.add_word('new').build.assoc('n').should == 'new'
+    @builder.build.keys.should include('new')
   end
 
   it "should be chainable" do
     @builder
       .add_word('cat')
       .add_word('car')
-      .class.should == Abbrev::Builder
+      .class.should == Builder
   end
 
   it "generates distinct abbreviations" do
@@ -45,4 +47,60 @@ describe Abbrev::Builder do
     abbrevs['car'].should == "car"
   end
 
+  # This part of the spec will have to change if I change my tree
+  # representation. Is including this in my spec a mistake? Or will it
+  # focus my refactoring better?
+  it "generates trees from strings" do
+    a = Builder.add_to_tree("a")
+    a.should == [['a',[nil]]]
+  end
+
+  it "Can incorporate new strings that share a prefix" do
+    a = Builder.add_to_tree("a")
+    Builder.add_to_tree("aa",a).should == [['a', [nil, ['a', [nil]] ] ]]
+    Builder.add_to_tree("ab",a)
+      .should == [['a', [nil, ['a', [nil]], ['b',[nil]]] ]]
+  end
+
+  it "Does not need to be initialized with single characters" do
+    states = Builder.add_to_tree("CA")
+    states.should == [['C', [['A',[nil]]] ]]
+    Builder.add_to_tree("CT",states)
+    states.should == [[ 'C', [['A',[nil]],['T',[nil]]] ]]
+
+    catcar = Builder.add_to_tree("cat")
+    catcar.should == [['c', [['a', [['t',[nil]]] ]] ]]
+    Builder.add_to_tree("car",catcar)
+      .should == [['c', [['a', [['t',[nil]], ['r',[nil]]] ]] ]]
+  end
+
+  it "incorporates longer words" do
+    expected = [['c', [['a', [['t', [nil,['s',[nil] ]] ]] ]] ]]
+
+    a = Builder.add_to_tree("cat")
+    Builder.add_to_tree("cats",a).should == expected
+  end
+
+  it "extends the tree idempotently" do
+    expected = [['c', [['a', [['r', [nil, ['t',[nil]]] ]] ]] ]]
+
+    a = Builder.add_to_tree("car")
+    Builder.add_to_tree("cart",a)
+    Builder.add_to_tree("cart",a).should == expected
+  end
+
+  it "incorporates sub-words" do
+    # have to show that both "car" and "cart are in the tree (for
+    # later traversal).
+    initial  = [[ 'c', [['a', [['r', [['t',[nil]]] ]] ]] ]]
+    expected = [[ 'c', [['a', [['r', [['t',[nil]],nil] ]] ]] ]]
+    a = Builder.add_to_tree("cart")
+    a.should == initial
+    Builder.add_to_tree("car",a).should == expected
+  end
+
+  it "builds abbreviations from trees" do
+    tree = [['c', [['a', [['r',[nil]],['t',[nil]]] ]] ]]
+    a = Builder.abbrevs_of(tree).assoc('car').should == 'car'
+  end
 end
